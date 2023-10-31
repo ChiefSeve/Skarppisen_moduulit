03ddd06059f8e4e73a677e1926c00f724873b046
Skarppinen <severi.karppinen@metropolia.fi>
2023-10-31 09:35:53 +0200

    Module 12 finished

--- moduulit/module12/second.py
+++ moduulit/module12/second.py
@@ -1,10 +1,12 @@
 import requests
 import json
 
-api_key = "2530563768b2b3b4e2f68a1fb113cc1f"
-lat = 10
-lon = 120
+api_key = 'foobar'
+city_name = input('Anna nimi: ')
+units = 'metric'
 
-call = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
+call = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}&units={units}'
 result = requests.get(call).json()
-print(json.dumps(result, indent=2))
+weather_main = result['main']
+weather_status = result['weather'][0]
+print(f'{weather_status["description"]}, {weather_main["temp"]} Celsius')
